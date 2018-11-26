f1916b86b0288f4f0d5d840e1d3021f015a02a7b
Tomas <tomasdo@lagash.com>
2018-11-26 17:56:11 -0300

    Primera version de pegarle a watson.

--- PetFinderBackOffice/PetFinderBackOffice/Controllers/ImagenMascotaController.cs
+++ PetFinderBackOffice/PetFinderBackOffice/Controllers/ImagenMascotaController.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using IBM.WatsonDeveloperCloud.Util;
 using IBM.WatsonDeveloperCloud.VisualRecognition.v3;
+using IBM.WatsonDeveloperCloud.VisualRecognition.v3.Model;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using PetFinderBackOffice.Models;
+using PetFinderBackOffice.ViewModels;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,22 +27,36 @@ namespace PetFinderBackOffice.Controllers
         private const string versionDate = "2018-03-19";
         private const string apikey = "HY_-KRN409tGl3X4Yp3zrbVxKpLGugfZ5HPr2gsCGMiC";
         private const string endpoint = "https://gateway.watsonplatform.net/visual-recognition/api";
-
-        // GET: api/<controller>
-        [HttpGet]
-        public IActionResult Get()
+        private static readonly List<string> classifierIds = new List<string>
         {
-            List<string> classifierIds = new List<string>
-            {
-                "DogsBreed_2053415849"
-            };
+            "DogsBreed_2053415849"
+        };
 
+        // POST api/<controller>
+        [HttpPost("/api/ImagenMascota/FotoEncontrado"), DisableRequestSizeLimit]
+        public IActionResult FotoEncontrado([FromBody]ImageFromServerModel imagenVM)
+        {
+            if (!Directory.Exists("ImagenesMascota"))
+            {
+                Directory.CreateDirectory("ImagenesMascota");
+            }
+
+            var img = Convert.FromBase64String(imagenVM.ImageURI);
+            System.IO.File.WriteAllBytes("ImagenesMascota//prueba.jpg", img);
+
+            var res = this.EnviarFotoAWatson("ImagenesMascota//prueba.jpg");
+
+            return this.Ok(res);
+        }
+
+        private string EnviarFotoAWatson(string path)
+        {
             VisualRecognitionService visualRecognition = new VisualRecognitionService();
             visualRecognition.SetEndpoint(endpoint);
-            visualRecognition.VersionDate = "2018-03-19";
+            visualRecognition.VersionDate = versionDate;
 
             FileStream img;
-            img = new FileStream("C:\\Siena\\fran.jpg", FileMode.Open);
+            img = new FileStream(path, FileMode.Open);
 
             TokenOptions iamAssistantTokenOptions = new TokenOptions()
             {
@@ -47,27 +67,9 @@ namespace PetFinderBackOffice.Controllers
             
             var result = visualRecognition.Classify(img, classifierIds: classifierIds);
 
-            JObject jsonResult = JObject.Parse(result.ResponseJson);
+            img.Close();
 
-            return this.Ok(result);
-        }
-
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return result.ResponseJson;
         }
     }
 }
---
+++ PetFinderBackOffice/PetFinderBackOffice/ViewModels/ImageFromServerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetFinderBackOffice.ViewModels
+{
+    public class ImageFromServerModel
+    {
+        public string ImageURI { get; set; }
+
+        //public string ImageName { get; set; }
+    }
+}
